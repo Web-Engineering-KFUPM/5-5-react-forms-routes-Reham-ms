19fd7c672e965521057689009c25f8b3376e778a
Reham-ms <s20227010@kfupm.edu.sa>
2026-03-02 10:34:15 +0300

    todo1 done

--- 5-5-react-route-form/src/App.jsx
+++ 5-5-react-route-form/src/App.jsx
@@ -144,24 +144,46 @@
 // - Use the hints above to guide your implementation, but write the actual JSX and logic yourself.
 // ================================================================
 
+import { Routes, Route, NavLink } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Registration from "./pages/Registration";
+import "./App.css";
 
 export default function App() {
-  return (
-    <div className="app">
-      <nav className="navbar">
-        <div className="brand">🧑‍💻 Student Portal</div>
-        <div className="links">
-          {/*Nav links*/}
+    return (
+        <div className="app">
+            <nav className="navbar">
+                <div className="brand">🧑‍💻 Student Portal</div>
+
+                <div className="links">
+                    {/* Nav links */}
+                    <NavLink to="/" end>
+                        Home
+                    </NavLink>
+
+                    <NavLink to="/about">
+                        About
+                    </NavLink>
+
+                    <NavLink to="/registration">
+                        Registration
+                    </NavLink>
+                </div>
+            </nav>
+
+            <main className="container">
+                {/* Routes */}
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/registration" element={<Registration />} />
+                </Routes>
+            </main>
+
+            <footer className="footer">
+                <span>© {new Date().getFullYear()} React Student Portal</span>
+            </footer>
         </div>
-      </nav>
-
-      <main className="container">
-        {/*Routes*/}
-      </main>
-
-      <footer className="footer">
-        <span>© {new Date().getFullYear()} React Student Portal</span>
-      </footer>
-    </div>
-  );
+    );
 }
